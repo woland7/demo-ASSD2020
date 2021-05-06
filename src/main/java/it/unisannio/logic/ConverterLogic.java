30e694db27d1ce96d680ff04696f96983fe0584d
woland7 <antonio.deiasio@gmail.com>
2021-05-06 19:35:41 +0200

    new version

--- src/main/java/it/unisannio/logic/ConverterLogic.java
+++ src/main/java/it/unisannio/logic/ConverterLogic.java
@@ -4,9 +4,10 @@ import javax.ejb.Stateless;
 
 @Stateless
 public class ConverterLogic implements ConverterLogicLocal {
-    private final float convFactor = 1.087f;
+    private final double convFactor = 1.087;
     @Override
     public double convertToDollar(double euro) {
+        System.out.println("sono qui"+euro*convFactor);
         return euro*convFactor;
     }
 }
